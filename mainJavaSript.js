53863ed2353888a45aba662b528e631a3190bcfb
Erens Oosthuizen <erensoosthuizen3@gmail.com>
2021-05-30 13:24:42 +0200

    First commit

---
+++ mainJavaSript.js
@@ -0,0 +1,15 @@
+
+// This function toggles the nav bar to show or hide
+function mobile_nav_bar(){
+    
+
+
+    var x = document.getElementById("mobileNav");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+}
+
+
